db67d5455d9d05472e8d73661cc7e041ed5e045f
wuly <wuly@channelsoft.com>
2018-10-11 10:15:59 +0800

    master测试冲突

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.set('view engine', 'ejs');
 router(app);
 
 var server = app.listen(3000, function () {
+    测试冲突
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
